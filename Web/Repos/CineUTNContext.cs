16867d0a9cf13ca97a4f13670051522166f9c2e1
Tomas Olocco <tomasolocco04@gmail.com>
2023-10-11 21:34:55 -0300

    olccotoputo22

---
+++ Web/Repos/CineUTNContext.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore;
+using Web.Repos.Models;
+
+namespace Web.Repos;
+
+public partial class CineUTNContext : DbContext
+{
+    public CineUTNContext()
+    {
+    }
+
+    public CineUTNContext(DbContextOptions<CineUTNContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Genero> Generos { get; set; }
+
+    //protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+    //    => optionsBuilder.UseSqlServer("name=conexion");
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
+}
